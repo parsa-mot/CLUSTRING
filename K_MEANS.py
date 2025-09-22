c81cb340b47cf34c1477f0a2118a63e82f18bee9
Parsa Motesharrei <motesharei.parsa@gmail.com>
2025-09-22 14:19:48 +0330

    Update K_MEANS.py

--- K_MEANS.py
+++ K_MEANS.py
@@ -19,6 +19,37 @@ plt.scatter(
 
 # Plot PCA-reduced cluster centers
 centers_pca = pca.transform(kmeans.cluster_centers_)
+
+
+
+
+# 2) elbow method
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.cluster import KMeans
+
+inertia = []
+K = range(1, 20)
+
+for k in K:
+    kmeans = KMeans(n_clusters=k, random_state=42)
+    kmeans.fit(df[float_cols])
+    inertia.append(kmeans.inertia_)
+
+plt.plot(K, inertia, "bo-")
+plt.xlabel("Number of clusters (k)")
+plt.ylabel("Inertia (SSE)")
+plt.title("Elbow Method")
+
+# Force all k values on x-axis
+plt.xticks(np.arange(1, 20, 1))
+
+# Add vertical dotted lines
+for k, val in zip(K, inertia):
+    plt.vlines(x=k, ymin=0, ymax=val, colors="gray", linestyles="dotted", alpha=0.6)
+
+plt.show()
+
 plt.scatter(
     centers_pca[:, 0], centers_pca[:, 1],
     c="red", s=200, alpha=0.75, marker="X"
